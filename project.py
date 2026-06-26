96482e9cd85ecb171a3476f08b0f23fc19e77506
Yuma Iwasawa <yuma1708314@gmail.com>
2026-06-26 19:30:18 +0900

    HoT: initial revision

---
+++ project.py
@@ -0,0 +1,22 @@
+import random
+
+
+def main():
+    loop_count = 3
+    heads_count = 0
+    print("\nTossing a coin...\n")
+    
+    for i in range(loop_count):
+        print(f"Round {i}: ", end="")
+        isHeads = random.randint(0, 1)
+        if isHeads: 
+            heads_count += 1
+            print("Heads")
+        else:
+            print("Tails")
+
+    print(f"\nHeads: {heads_count}, Tails: {loop_count - heads_count}\n")
+
+    
+if __name__ == "__main__":
+    main()
