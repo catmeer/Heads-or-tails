c21fdc7f109ed7b88949aec38cb4cdd5f620f5ef
Yuma Iwasawa <yuma1708314@gmail.com>
2026-06-28 13:24:36 +0900

    Victory message added

--- project.py
+++ project.py
@@ -16,6 +16,7 @@ def main():
             print("Tails")
 
     print(f"\nHeads: {heads_count}, Tails: {loop_count - heads_count}\n")
+    print(f"You {'won' if heads_count > (loop_count - heads_count) else 'lost'}")
 
     
 if __name__ == "__main__":
